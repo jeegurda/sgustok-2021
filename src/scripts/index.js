effc73f14fd8fa250533488bb924b835337a1ab1
Shagen A <asriyan.sh@gmail.com>
2021-03-27 16:48:29 +0300

    use splide

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,14 @@
 const initApp = () => {
-
+  new window.Splide('.splide', {
+    type: 'loop',
+    arrows: false,
+    pagination: false,
+    direction: 'ttb',
+    autoHeight: true,
+    heightRatio: 1
+  }).mount()
 }
 
-initApp()
+document.addEventListener('DOMContentLoaded', () => {
+  initApp()
+})
