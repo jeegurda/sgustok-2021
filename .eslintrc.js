c1abeecd1f71b50820a8f9d07c63afbad0cd8d93
Shagen A <asriyan.sh@gmail.com>
2021-03-27 12:04:21 +0300

    initial structure

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'eol-last': 0,
+    'no-trailing-spaces': 0
+  }
+}
---
+++ src/scripts/index.js
@@ -0,0 +1,5 @@
+const initApp = () => {
+
+}
+
+initApp()
