f2889ce87010726c0ffb18cb2781cd700091182a
Shagen A <asriyan.sh@gmail.com>
2021-03-27 18:36:58 +0300

    use swiper's append method

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,69 @@
-const initApp = () => {
-  new window.Splide('.splide', {
-    type: 'loop',
-    arrows: false,
-    pagination: false,
-    direction: 'ttb',
-    autoHeight: true,
-    heightRatio: 1
-  }).mount()
+const getJson = async () => {
+  const json = await fetch('../content/content.json')
+    .then(res => {
+      if (res.ok) {
+        return res.json()
+      }
+      return Promise.reject(res)
+    })
+    .catch(reason => {
+      console.error('Failed to get JSON', reason)
+      return Promise.reject(reason)
+    })
+
+  return json
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  initApp()
+const createSlider = () => {
+  return new window.Swiper('.swiper-container', {
+    direction: 'vertical',
+    loop: true,
+    grabCursor: true,
+    longSwipesRatio: 0.1,
+    longSwipesMs: 100,
+    mousewheel: {
+      forceToAxis: true,
+      thresholdDelta: 20,
+      thresholdTime: 100
+    },  
+    hashNavigation: {
+      replaceState: true,
+      watchState: true
+    },
+    preloadImages: false,
+    lazy: {
+      checkInView: true,
+      scrollingElement: '.swiper-container'
+    },
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    slidesPerView: 1,
+    init: false
+  })
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const content = await getJson()
+
+    console.log(content)
+    const slider = createSlider()
+
+    const slideTemplate = `
+      <div class="swiper-slide swiper-lazy project" data-hash="%id" data-background="%src">
+
+      </div>    
+    `
+    slider.init()
+
+    content.forEach(item => {
+      slider.appendSlide(
+        slideTemplate
+          .replace('%src', item.image)
+          .replace('%id', item.id)
+      )
+    })
+  } catch (reason) {
+    console.error('Failed to load content', reason)
+  }
 })
